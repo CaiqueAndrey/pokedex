1ede184b99343504d6910ed0484a9bf99a40df40
Caique <caiqueandrey@hotmail.com>
2022-07-31 18:37:34 -0300

    Initial commit

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -4,36 +4,42 @@ import Pokemon from './Pokemon';
 
 export function Pokedex() {
 
-    // const [pokemons, setPokemons] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const pokemons = [];
-    
-    const getPokemons = async (id: number) => {
-        try {
-            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-            let pokemon = await response.json();
-            console.log('pokemono', typeof pokemon);
-            console.log('pokemono', pokemons);
-            return pokemons.push(pokemon);
-          } catch (error) {
-            console.log(error);
-          }
-    }
+    const [pokemons, setPokemons] = useState([]);
+    const [loading, setLoading] = useState(false);
+    //const pokemons = [];
+    const types = [{type:{name:'fire'}},{type:{name:'water'}},{type:{name:'grass'}}];
 
     useEffect(()=> {
-        for (let index = 1; index <= 2; index++) {
-            getPokemons(index);
-            if(index === 2)
-                setLoading(false);
-          }
-    }, [pokemons])
+        const getPokemons = async (id: number) => {
+            setLoading(true);
+            try {
+                let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+                let pokemon = await response.json();
+
+                // console.log('pokemono', pokemons);
+                return setPokemons({...pokemon});
+              } catch (error) {
+                console.log(error);
+              }
+        }
+
+        // for (let index = 1; index <= 2; index+= 1) {
+        //     //getPokemons(index);
+        //     if(index === 2){
+        //         setLoading(false);
+        //     }
+
+        //     console.log('index->',index);
+        // }
+    }, []);
 
     return (
         <>
             <Div>
-                <>
+                {/* <>
                     { loading && <div>Carregando...</div> }
                     {
+                        pokemons.length >  0 &&
                         pokemons.map((pokemon, index) => {
                             console.log('pokemonmap--->', pokemon[index].name);
                             return (
@@ -46,7 +52,14 @@ export function Pokedex() {
                             )
                         })
                     }
-                </>
+                </> */}
+                
+                <Pokemon 
+                types={types}
+                pokemonId={'4'}
+                pokemonName={'Charmander'}
+                key={1}
+                    />
             </Div>
         </>
     );
